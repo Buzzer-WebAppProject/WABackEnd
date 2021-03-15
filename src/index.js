465358a00403e717115cb2d0a5ec6ff37b7d1214
David <60017346+DavidMaglica@users.noreply.github.com>
2021-03-15 12:34:05 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import storage from './memory_storage.js'
+import cors from 'cors'
+
+const app = express()  // instanciranje aplikacije
+const port = 3000  // port na kojem će web server slušati
+
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+    let posts = storage.posts
+    let query = req.query
+    
+    if (query.title) {
+        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
+    }
+    
+    if (query.createdBy) {
+        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
+    }
+    
+    if (query._any) {
+        let terms = query._any.split(" ")
+        posts = posts.filter(doc => {
+            let info = doc.title + " " + doc.createdBy
+            return terms.every(term => info.indexOf(term) >= 0)
+        })
+    }
+
+    res.json(posts)
+})
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
