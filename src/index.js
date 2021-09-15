4686a9dc75e97cb5a5239fb5d491f902b44071dc
DavidMaglica <davidmaglica@protonmail.ch>
2021-09-15 16:24:21 +0200

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import MongoClient from "mongodb/lib/mongo_client";
 import mongo from "mongodb";
 
 const app = express(); // instanciranje aplikacije
-const port = 3000; // port na kojem će web server slušati
+const port = process.ev.port || 3000; // port na kojem će web server slušati
 
 app.use(cors());
 app.use(express.json());
